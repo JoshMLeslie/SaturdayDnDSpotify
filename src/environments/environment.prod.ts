e5aab7a8e7792c236931142b03931f0bc5cfacd2
Josh Leslie <hello@joshmleslie.com>
2019-04-03 22:21:13 -0400

    more config updates -- pushing to github pages now works

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
   , spotifyDomain: 'https://api.spotify.com/v1'
   , spotifyClientId: 'fb5261254a374fd7a6ed0be556013ff9'
-  , appDomain: 'https://saturday-dnd.herokuapp.com/'
+  , appDomain: 'https://joshmleslie.github.io/SaturdayDnDSpotify/'
 };
